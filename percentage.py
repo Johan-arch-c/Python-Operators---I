6f80d19410d9a6054ea75f5879d6242176489a31
Johan-arch-c <johannithya@gmail.com>
2025-07-01 16:52:58 +1000

    percentage

---
+++ percentage.py
@@ -0,0 +1,9 @@
+print("enter the marks obtained in the following subjects")
+math=int(input("enter the marks obtained in math"))
+science=int(input("enter the marks obtained in science"))
+english=int(input("enter the marks obtained in english"))
+art=int(input("enter the marks obtained in art"))
+sum=math+science+english+art
+print(sum)
+percentage=(sum/400)*100
+print(percentage)
